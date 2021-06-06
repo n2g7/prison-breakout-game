4ea4bb30ad3e3c1ea6c7a173cd70de26d5e3c79d
Positron11 <aarushbeleren11@gmail.com>
2021-06-06 12:06:49 +0530

    CREATE CONTROLLER CLASS

--- classes.py
+++ classes.py
@@ -9,6 +9,18 @@ class Player:
 		self.name = name
 		self.prison = prison
 
+	def __str__(self) -> str:
+		return self.name
+
+	def move(self, new_room):
+		if new_room in self.prison.rooms:
+			for room in self.prison.rooms.values():
+				if self in room.occupants:
+					room.occupants.remove(self)
+			self.prison.rooms[new_room].occupants.append(self)
+		else:
+			print("No such room")
+		
 
 # Inmate class
 class Inmate:
@@ -19,6 +31,9 @@ class Inmate:
 		self.strength = strength
 		self.friendliness = friendliness
 
+	def __str__(self) -> str:
+		return self.name
+
 
 # Prison guard class
 class Guard:
@@ -28,12 +43,51 @@ class Guard:
 		self.prison = prison
 		self.friendliness = friendliness
 
+	def __str__(self) -> str:
+		return self.number
+
 
 # Prison class
 class Prison:
 
 	def __init__(self, room_names) -> None:
-		self.rooms = {name: {"occupants": [], "assets": []} for name in room_names}
+		self.rooms = {name:Room(name, [], []) for name in room_names}
 
 	def __str__(self) -> str:
 		return "Adironacks Correctional Facility"
+
+
+# Room class
+class Room:
+
+	def __init__(self, name, assets, occupants) -> None:
+		self.name = name
+		self.assets = assets
+		self.occupants = occupants
+
+	def __str__(self) -> str:
+		return self.name
+
+
+# Controller class
+class Controller:
+
+	def __init__(self, player) -> None:
+		self.player = player
+
+	def get_input(self):
+		player_input = input("Input: ").lower().strip().split(" ")
+		try:
+			if len(player_input) == 1:
+				getattr(self, player_input[0])()
+			elif len(player_input) == 2:
+				getattr(self, player_input[0])(player_input[1])
+			else:
+				print("Invalid arguments")
+		except TypeError:
+			print("Invalid arguments")
+		except AttributeError:
+			print("Invalid command")
+	
+	def goto(self, location):
+		self.player.move(location)
--- main.py
+++ main.py
@@ -6,9 +6,16 @@ import storydata
 guard_count = 10
 
 # Initialize prison
-prison = classes.Prison(["Cells", "Cafeteria", "Yard", "Laundry", "Library", "Solitary", "Office"])
+prison = classes.Prison(["cells", "cafeteria", "yard", "laundry", "library", "solitary", "office"])
 
 # Initialize player and characters
 player = classes.Player("Andy", prison)
 guards = {number:classes.Guard(number, prison, random.randint(0,100)) for number in range(1, guard_count + 1)}
 inmates = {name:classes.Inmate(name, prison, storydata.inmates[name]["strength"], storydata.inmates[name]["friendliness"]) for name in storydata.inmates.keys()}
+
+# Initialize controller
+controller = classes.Controller(player)
+
+# Initialize character positions
+prison.rooms["cells"].occupants.append(player)
+prison.rooms["cells"].occupants.extend(list(inmates.values()))
