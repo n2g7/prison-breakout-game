c448a5befe2f6e9e87c9743b1d6bb595f297f318
Positron11 <aarushbeleren11@gmail.com>
2021-06-06 10:24:48 +0530

    CREATE BASIC PRISON CLASS

--- classes.py
+++ classes.py
@@ -5,15 +5,17 @@ class Player:
 	static_inventory = []
 	mobile_inventory = []
 
-	def __init__(self, name) -> None:
+	def __init__(self, name, prison) -> None:
 		self.name = name
+		self.prison = prison
 
 
 # Inmate class
 class Inmate:
 
-	def __init__(self, name, strength, friendliness) -> None:
+	def __init__(self, name, prison, strength, friendliness) -> None:
 		self.name = name
+		self.prison = prison
 		self.strength = strength
 		self.friendliness = friendliness
 
@@ -21,6 +23,17 @@ class Inmate:
 # Prison guard class
 class Guard:
 
-	def __init__(self, number, friendliness) -> None:
+	def __init__(self, number, prison, friendliness) -> None:
 		self.number = number
+		self.prison = prison
 		self.friendliness = friendliness
+
+
+# Prison class
+class Prison:
+
+	def __init__(self, room_names) -> None:
+		self.rooms = {name: {"occupants": [], "assets": []} for name in room_names}
+
+	def __str__(self) -> str:
+		return "Adironacks Correctional Facility"
--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import random
 import classes
 import storydata
 
+# Initialize variables
+guard_count = 10
+
+# Initialize prison
+prison = classes.Prison(["Cells", "Cafeteria", "Yard", "Laundry", "Library", "Solitary", "Office"])
+
 # Initialize player and characters
-player = classes.Player("Andy")
-guards = {number:classes.Guard(number, random.randint(0,100)) for number in range(1,11)}
-inmates = {name:classes.Inmate(name, storydata.inmates[name]["strength"], storydata.inmates[name]["friendliness"]) for name in storydata.inmates.keys()}
+player = classes.Player("Andy", prison)
+guards = {number:classes.Guard(number, prison, random.randint(0,100)) for number in range(1, guard_count + 1)}
+inmates = {name:classes.Inmate(name, prison, storydata.inmates[name]["strength"], storydata.inmates[name]["friendliness"]) for name in storydata.inmates.keys()}
