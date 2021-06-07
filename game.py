9faf565d99739297ab19cfb18eb14712aafb75d0
Positron11 <aarushbeleren11@gmail.com>
2021-06-07 09:03:06 +0530

    USE KWARGS
    Why didn't I think of this before

--- game.py
+++ game.py
@@ -2,43 +2,14 @@ import gamedata
 import gameclasses
 
 
-prison = gameclasses.Prison(
-	rooms=[gameclasses.Room(
-		name=room["name"]
-	) for room in gamedata.rooms]
-)
+prison = gameclasses.Prison(rooms=[gameclasses.Room(**room_data) for room_data in gamedata.rooms])
+schedule = gameclasses.Schedule(events=[gameclasses.Event(**event_data) for event_data in gamedata.schedule])
 
-schedule = gameclasses.Schedule(
-	events=[gameclasses.Event(
-		location=event["location"],
-		duration=event["duration"]
-	) for event in gamedata.schedule]
-)
-
-player = gameclasses.Player(
-	name="Andy", 
-	prison=prison,
-	strength=100, 
-	popularity=0
-)
+player = gameclasses.Player(name="Andy", prison=prison, strength=100, popularity=0)
+inmates = [gameclasses.Inmate(**inmate_data) for inmate_data in gamedata.inmates]
+guards = [gameclasses.Guard(**guard_data) for guard_data in gamedata.guards]
 
 controller = gameclasses.Controller(player)
-
-
-inmates = [gameclasses.Inmate(
-	name=inmate["name"],
-	strength=inmate["strength"],
-	friendliness=inmate["friendliness"],
-	influence=inmate["influence"]
-) for inmate in gamedata.inmates]
-
-
-guards = [gameclasses.Guard(
-	name=guard["name"],
-	friendliness=guard["friendliness"]
-) for guard in gamedata.guards]
-
-
 
 while True:
 	# print(" ".join([f"[{room.name}: {', '.join([inmate.name for inmate in room.occupants]) if room.get_occupants() else 'Empty'}]" for room in prison.rooms]))
